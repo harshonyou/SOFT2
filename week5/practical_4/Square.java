de79d50790645261fb2b1f4596175faf887a22f7
Harsh Mohan <harshmohan813@gmail.com>
2021-02-17 20:11:39 +0000

    r n c

---
+++ week5/practical_4/Square.java
@@ -0,0 +1,31 @@
+package week5.practical_4;
+
+public class Square extends Rectangle {
+
+    public Square(){
+        super();
+    }
+    public Square(double side){
+        super(side,side);
+    }
+    public Square(double side, String color, boolean filled){
+        super(side, side, color, filled);
+    }
+
+    @Override
+    public String toString() {
+        return "A Square with side="+super.getLength()+", which is a subclass of "+super.toString();
+    }
+
+    @Override
+    public void setLength(double length) {
+        super.setLength(length);
+        super.setWidth(length);
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+        super.setLength(length);
+    }
+}
