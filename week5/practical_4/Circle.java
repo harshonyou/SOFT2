9b1cf83e254b6b74645d39b535052bbe4101db1e
Harsh Mohan <harshmohan813@gmail.com>
2021-02-21 23:15:41 +0000

    Create Circle.java

---
+++ week5/practical_4/Circle.java
@@ -0,0 +1,37 @@
+package week5.practical_4;
+
+public class Circle extends Shape {
+    public double radius;
+
+    public Circle(){
+        radius = 1.0;
+    }
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    public Circle(double radius, String color, boolean filled){
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return Math.PI*Math.pow(this.radius, 2);
+    }
+
+    public double getPerimeter(){
+        return 2*Math.PI*this.radius;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius="+this.radius+", which is a subclass of "+super.toString();
+    }
+}
