5f373f32a293eb91395ff06b6e9e50b3c00af262
Harsh Mohan <harshmohan813@gmail.com>
2021-02-28 17:41:14 +0000

    week4 done

---
+++ week5/practical_4/E1.java
@@ -0,0 +1,5 @@
+package week5.practical_4;
+
+public class E1 {
+
+}
---
+++ week5/practical_4/Polygon.java
@@ -0,0 +1,26 @@
+package week5.practical_4;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Polygon extends Point{
+    public ArrayList<Point> points;
+    public int size;
+
+    public Polygon(int sides){
+        this.size=sides;
+        points = new ArrayList<Point>(sides);
+
+    }
+
+    public void Initialize(){
+        Scanner scan = new Scanner(System.in);
+        int x, y;
+        for(int i=0; i<size; i++){
+            System.out.println("Enter ");
+            x = scan.nextInt();
+            y = scan.nextInt();
+            points.add(new Point(x, y));
+        }
+        scan.close();
+    }
+}
