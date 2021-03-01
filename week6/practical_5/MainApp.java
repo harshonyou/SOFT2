76eec084ff09aa13102cbb8ed530a605a518e812
Harsh Mohan <harshmohan813@gmail.com>
2021-03-01 14:43:56 +0000

    week 6 done

---
+++ week6/practical_5/MainApp.java
@@ -0,0 +1,12 @@
+package week6.practical_5;
+
+public class MainApp extends TallyCounter {
+    public static void main(String[] args) {
+        TallyCounter tallyCount = new TallyCounter(4);
+        System.out.println(tallyCount);
+        for(int i=0; i<9999+1; i++){
+            tallyCount.increment();
+        }
+        System.out.println(tallyCount.read());
+    }
+}
